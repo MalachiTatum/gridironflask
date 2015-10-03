1839f2ad539bd2dd11bfd2f61da6401668ff4ca1
Salil Doshi <doshililas@gmail.com>
2015-10-03 14:30:02 -0400

    hn feeder

--- app.py
+++ app.py
@@ -1,9 +1,60 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect, url_for, flash, request, abort, send_file 
+from hackernews import HackerNews
+from flask_material import Material
+from flask.ext.wtf import Form
+from wtforms import TextField, HiddenField, ValidationError, RadioField,\
+    BooleanField, SubmitField, IntegerField, FormField, SelectField, StringField, RadioField,  validators
+from metamind.api import ClassificationModel, set_api_key
+from IPython import embed
+
+
+  
 app = Flask(__name__)
+Material(app)
+
+hn = HackerNews()
+
+set_api_key("39W42KN7ZKFYcWewQLewjxIWd8wfsICtcbyD8SxQZC42gEvGmA")
+
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index') 
+def index():  
+  
+    
+  
+  def byteify(input):
+    if isinstance(input, dict):
+        return {byteify(key):byteify(value) for key,value in input.iteritems()}
+    elif isinstance(input, list):
+        return [byteify(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
+      
+  stories = {}
+  posts = []
+  x=0
+  for story_id in hn.top_stories(limit=10):
+    story = hn.get_item(story_id)
+    stories[x] = {}
+    stories[x]['name'] = [story.title]
+    stories[x]['score'] = [story.score]
+    x+=1
+  
+  stories = byteify(stories)
+  
+  for x in stories:
+    posts.append(stories[x])
+      
+  
+  sentiment = byteify(ClassificationModel(id=155).predict("hackathon", input_type="text"))
+
+
+  return render_template('index.html', title='HN Feeder', posts=posts, user='Salil')
+                           
 
 if __name__ == '__main__':
     app.run()
