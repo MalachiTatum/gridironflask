60d7233e91c9ecb5996d7d16c40658e40d9b09a0
Malachi Tatum <tatum_malachi@bah.com>
2015-10-03 14:55:06 -0400

    debugging sh files

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 ./build.sh
 #docker run --name postgres-db -p 5432:5432 -d postgres
 #docker run -p 5000:5000 --link postgres-db:db -e APP_SETTINGS="config.DevDockerConfig" -d flask
-docker run -p 5000:5000 -d flask
+docker run -p 5000:5000 flask
