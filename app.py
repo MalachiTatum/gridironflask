c3fe2078510f97f17d12b364f42ed32166345d97
Salil Doshi <doshililas@gmail.com>
2015-10-03 16:02:04 -0400

    js bullshit

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def index():
   posts = []
   x=0
   #get the top 10 stories on HN, push into a dict with name and score
-  for story_id in hn.top_stories(limit=10):
+  for story_id in hn.top_stories(limit=50):
     story = hn.get_item(story_id)
     stories[x] = {}
     stories[x]['name'] = [story.title]
