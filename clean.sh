7f4b79ed34eea673a3d3a69b4bc4fd131c69e16b
Ubuntu <ubuntu@ip-172-31-64-76.ec2.internal>
2015-10-03 15:16:12 +0000

    added sudo to stuff

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
-sudo docker ps -q | xargs docker stop # use rm -f to force.
-sudo docker ps --no-trunc -aq | xargs docker rm 
+sudo docker ps -q | xargs sudo docker stop # use rm -f to force.
+sudo docker ps --no-trunc -aq | xargs sudo docker rm 
